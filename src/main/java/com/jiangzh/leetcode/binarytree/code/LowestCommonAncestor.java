42e81ca4be0ad4c98d7f4326f3d236257bd7f6e9
jiangzheng3 <jiangzheng3@xiaomi.com>
2022-01-21 15:41:20 +0800

    二叉树之公共祖先

--- src/main/java/com/jiangzh/leetcode/binarytree/code/LowestCommonAncestor.java
+++ src/main/java/com/jiangzh/leetcode/binarytree/code/LowestCommonAncestor.java
@@ -5,25 +5,33 @@ import com.jiangzh.leetcode.binarytree.base.TreeNode;
 
 public class LowestCommonAncestor {
 
-    private boolean hasNode = false;
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null){
             return null;
         }
 
-        return order(root,p,q);
-    }
+        if(root.val == p.val || root.val == q.val){
+            return root;
+        }
 
-    private TreeNode order(TreeNode node,TreeNode p,TreeNode q){
-        if(node == null){
+        TreeNode n1 = lowestCommonAncestor(root.left,p,q);
+        TreeNode n2 = lowestCommonAncestor(root.right,p,q);
+
+        if(n1 != null && n2 != null){
+            return root;
+        }
+
+        if(n1 == null && n2 == null){
             return null;
         }
 
-        // TODO
+        if(n1 == null || n2 == null){
+            return n1 == null ? n2 : n1;
+        }
+
+        System.out.println("判断条件不全");
         return null;
     }
-
 
     public static void main(String[] args) {
         Integer[] input = new Integer[]{3,5,1,6,2,0,8,null,null,7,4};
